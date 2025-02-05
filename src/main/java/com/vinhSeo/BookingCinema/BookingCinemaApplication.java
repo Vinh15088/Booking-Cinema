5aa13d98f9d9dc77da0e58a4ac40b8389b0cb621
Vinh15088 <vinhzz1508@gmail.com>
2025-02-05 15:39:27 +0700

    Init project

---
+++ src/main/java/com/vinhSeo/BookingCinema/BookingCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.vinhSeo.BookingCinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingCinemaApplication.class, args);
+	}
+
+}

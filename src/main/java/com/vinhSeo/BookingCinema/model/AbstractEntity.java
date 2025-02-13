1a2ea8234fca8a49ee7fb86a8af21440c1e8a933
Vinh15088 <vinhzz1508@gmail.com>
2025-02-13 10:33:29 +0700

    abstract common for entities

---
+++ src/main/java/com/vinhSeo/BookingCinema/model/AbstractEntity.java
@@ -0,0 +1,27 @@
+package com.vinhSeo.BookingCinema.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class AbstractEntity<T> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private T id;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    Date createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    Date updatedAt;
+}

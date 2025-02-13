f1d570b8a648ef58547537d007ea671484a3b8e9
Vinh15088 <vinhzz1508@gmail.com>
2025-02-13 10:29:59 +0700

    add enums for user

---
+++ src/main/java/com/vinhSeo/BookingCinema/enums/Gender.java
@@ -0,0 +1,5 @@
+package com.vinhSeo.BookingCinema.enums;
+
+public enum Gender {
+    MALE, FEMALE, OTHER;
+}
---
+++ src/main/java/com/vinhSeo/BookingCinema/enums/TokenType.java
@@ -0,0 +1,6 @@
+package com.vinhSeo.BookingCinema.enums;
+
+public enum TokenType {
+    ACCESS_TOKEN,
+    REFRESH_TOKEN,
+}

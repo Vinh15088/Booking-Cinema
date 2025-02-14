60ba6c3d80a57a82a27aa5a5494c80ad6a7a2ea1
Vinh15088 <vinhzz1508@gmail.com>
2025-02-14 23:11:53 +0700

    update for redis token, mail

--- src/main/java/com/vinhSeo/BookingCinema/controller/AuthenticationController.java
+++ src/main/java/com/vinhSeo/BookingCinema/controller/AuthenticationController.java
@@ -3,9 +3,12 @@ package com.vinhSeo.BookingCinema.controller;
 import com.vinhSeo.BookingCinema.dto.request.UserLoginRequest;
 import com.vinhSeo.BookingCinema.dto.response.DataApiResponse;
 import com.vinhSeo.BookingCinema.dto.response.TokenResponse;
+import com.vinhSeo.BookingCinema.model.RedisToken;
 import com.vinhSeo.BookingCinema.service.AuthenticationService;
+import com.vinhSeo.BookingCinema.service.RedisTokenService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -13,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("/auth")
@@ -20,9 +24,11 @@ import java.util.Date;
 @Slf4j(topic = "AUTHENTICATION_CONTROLLER")
 @Tag(name = "Authentication Controller")
 public class AuthenticationController {
-    private final AuthenticationService authenticationService;
 
-    @PostMapping
+    private final AuthenticationService authenticationService;
+    private final RedisTokenService redisTokenService;
+
+    @PostMapping("/login")
     @Operation(method = "POST", summary = "Login and generate access & refresh tokens",
             description = "Send a request via this API to login by user")
     public ResponseEntity<?> getAccessToken(@RequestBody UserLoginRequest userLoginRequest) throws Exception {
@@ -44,10 +50,10 @@ public class AuthenticationController {
     @PostMapping("/refresh")
     @Operation(method = "POST", summary = "Refresh access token using refresh token",
             description = "Send a request via this API to refresh token")
-    public ResponseEntity<?> getRefreshToken(@RequestParam String refreshToken) throws Exception {
+    public ResponseEntity<?> getRefreshToken(HttpServletRequest request) throws Exception {
         log.info("Refresh token");
 
-        TokenResponse tokenResponse = authenticationService.getRefreshToken(refreshToken);
+        TokenResponse tokenResponse = authenticationService.getRefreshToken(request);
 
         DataApiResponse dataApiResponse = DataApiResponse.builder()
                 .success(true)
@@ -55,6 +61,61 @@ public class AuthenticationController {
                 .timestamp(new Date())
                 .message("Refresh token successfully")
                 .data(tokenResponse)
+                .build();
+
+        return ResponseEntity.ok().body(dataApiResponse);
+    }
+
+    @PostMapping("/remove")
+    @Operation(method = "POST", summary = "Remove token",
+            description = "Send a request via this API to remove token")
+    public ResponseEntity<?> removeToken(HttpServletRequest request) throws Exception {
+        log.info("Remove token");
+
+        String result = authenticationService.removeToken(request);
+
+        DataApiResponse dataApiResponse = DataApiResponse.builder()
+                .success(true)
+                .code(HttpStatus.CREATED.value())
+                .timestamp(new Date())
+                .message(result)
+                .build();
+
+        return ResponseEntity.ok().body(dataApiResponse);
+    }
+
+    /* Test all token in redis */
+    @GetMapping("/list")
+    public ResponseEntity<?> getAllTokenInRedis() {
+        log.info("Get all tokens in redis");
+
+        List<RedisToken> redisTokens = redisTokenService.findAll();
+
+        DataApiResponse dataApiResponse = DataApiResponse.builder()
+                .success(true)
+                .code(HttpStatus.OK.value())
+                .timestamp(new Date())
+                .message("Get all tokens in redis successfully")
+                .data(redisTokens)
+                .build();
+
+        return ResponseEntity.ok().body(dataApiResponse);
+    }
+
+    @GetMapping("/confirm-mail")
+    @Operation(method = "GET", summary = "Confirm mail",
+            description = "Send a request via this API to confirm mail")
+    public ResponseEntity<?> confirmEmail(@RequestParam String id,
+                                          @RequestParam String email) {
+        log.info("Confirm mail");
+
+        authenticationService.confirmMail(id, email);
+
+        DataApiResponse dataApiResponse = DataApiResponse.builder()
+                .success(true)
+                .code(HttpStatus.OK.value())
+                .timestamp(new Date())
+                .message("Confirm mail successfully")
                 .build();
 
         return ResponseEntity.ok().body(dataApiResponse);
--- src/main/java/com/vinhSeo/BookingCinema/service/AuthenticationService.java
+++ src/main/java/com/vinhSeo/BookingCinema/service/AuthenticationService.java
@@ -3,10 +3,18 @@ package com.vinhSeo.BookingCinema.service;
 import com.vinhSeo.BookingCinema.dto.request.UserLoginRequest;
 import com.vinhSeo.BookingCinema.dto.response.TokenResponse;
 import com.vinhSeo.BookingCinema.enums.TokenType;
+import com.vinhSeo.BookingCinema.enums.UserStatus;
 import com.vinhSeo.BookingCinema.exception.AppException;
 import com.vinhSeo.BookingCinema.exception.ErrorApp;
+import com.vinhSeo.BookingCinema.exception.InvalidDataException;
+import com.vinhSeo.BookingCinema.exception.ResourceNotFoundException;
+import com.vinhSeo.BookingCinema.model.RedisMailConfirm;
+import com.vinhSeo.BookingCinema.model.RedisToken;
 import com.vinhSeo.BookingCinema.model.User;
+import com.vinhSeo.BookingCinema.repository.RedisMailConfirmRepository;
 import com.vinhSeo.BookingCinema.repository.UserRepository;
+import io.micrometer.common.util.StringUtils;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,6 +26,9 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.springframework.http.HttpHeaders.REFERER;
+
+
 @Service
 @Slf4j(topic = "AUTHENTICATION_SERVICE")
 @RequiredArgsConstructor
@@ -25,6 +36,8 @@ public class AuthenticationService {
     private final UserRepository userRepository;
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
+    private final RedisTokenService redisTokenService;
+    private final RedisMailConfirmRepository redisMailConfirmRepository;
 
     public TokenResponse getAccessToken(UserLoginRequest userLoginRequest) throws Exception {
         log.info("Get access token");
@@ -53,17 +66,27 @@ public class AuthenticationService {
         String accessToken = jwtService.generateAccessToken(userLoginRequest.getUsername(), authorities);
         String refreshToken = jwtService.generateRefreshToken(userLoginRequest.getUsername(), authorities);
 
+        // save token with redis
+        RedisToken redisToken = RedisToken.builder()
+                .id(userLoginRequest.getUsername())
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+        redisTokenService.save(redisToken);
+
         return TokenResponse.builder()
                 .accessToken(accessToken)
                 .refreshToken(refreshToken)
                 .build();
     }
 
-    public TokenResponse getRefreshToken(String refreshToken) throws Exception {
+    public TokenResponse getRefreshToken(HttpServletRequest request) throws Exception {
         log.info("Get refresh token");
 
+        String refreshToken = request.getHeader(REFERER);
+
         if(refreshToken == null || refreshToken.isEmpty()) {
-            throw new Exception("Refresh token is empty");
+            throw new InvalidDataException("Refresh token is empty");
         }
 
         try {
@@ -76,13 +99,70 @@ public class AuthenticationService {
 
             String accessToken = jwtService.generateAccessToken(username, authorities);
 
+            // save token with redis
+            RedisToken redisToken = RedisToken.builder()
+                    .id(user.getUsername())
+                    .accessToken(accessToken)
+                    .refreshToken(refreshToken)
+                    .build();
+            redisTokenService.save(redisToken);
+
             return TokenResponse.builder()
                     .accessToken(accessToken)
                     .refreshToken(refreshToken)
                     .build();
         } catch (Exception e) {
             log.info("Refresh token failed: {}", e.getMessage());
-            throw new Exception(e.getMessage());
+            throw new InvalidDataException(e.getMessage());
+        }
+    }
+
+    public String removeToken(HttpServletRequest request) throws Exception {
+        log.info("Remove token");
+
+        String token = request.getHeader(REFERER);
+        if(StringUtils.isBlank(token)) {
+            throw new InvalidDataException("Token must be not blank");
+        }
+
+        String username = jwtService.extractUsername(token, TokenType.ACCESS_TOKEN);
+
+        if(!redisTokenService.checkExisted(username)) {
+            throw new InvalidDataException("Token is not found in Redis DB");
+        }
+
+        redisTokenService.remove(username);
+
+        return "Remove token successfully";
+    }
+
+    public void confirmMail(String id, String email) {
+        log.info("Confirm mail");
+
+        RedisMailConfirm redisMailConfirm = redisMailConfirmRepository.findById(id).orElseThrow(() -> {
+                log.info("RedisMailConfirm not found");
+                return new ResourceNotFoundException("Redis mail confirm is not found");
+            });
+
+        if(redisMailConfirm.getEmail().equals(email)) {
+            log.info("Confirm mail already exist");
+
+            if(userRepository.findByEmail(email) == null) {
+                throw new AppException(ErrorApp.USER_NOT_FOUND);
+            }
+
+            User user = userRepository.findByEmail(email);
+            log.info("Verify user with email: {}, have full name: {}", email, user.getFullName());
+            user.setUserStatus(UserStatus.ACTIVE);
+
+            userRepository.save(user);
+
+            log.info("Confirm mail successfully");
+
+            redisMailConfirmRepository.delete(redisMailConfirm);
+        } else {
+            log.info("Confirm mail fail");
+            throw new InvalidDataException("Confirm mail fail");
         }
     }
 }

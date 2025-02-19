3c179acf13373b6640eeb099e57f1bea345814e5
Vinh15088 <vinhzz1508@gmail.com>
2025-02-20 00:47:55 +0700

    add data itit for role

--- src/main/java/com/vinhSeo/BookingCinema/utils/DataInitializer.java
+++ src/main/java/com/vinhSeo/BookingCinema/utils/DataInitializer.java
@@ -1,8 +1,10 @@
 package com.vinhSeo.BookingCinema.utils;
 
 import com.vinhSeo.BookingCinema.enums.RoomTypeStatus;
+import com.vinhSeo.BookingCinema.model.Role;
 import com.vinhSeo.BookingCinema.model.RoomType;
 import com.vinhSeo.BookingCinema.model.SeatType;
+import com.vinhSeo.BookingCinema.repository.RoleRepository;
 import com.vinhSeo.BookingCinema.repository.RoomTypeRepository;
 import com.vinhSeo.BookingCinema.repository.SeatTypeRepository;
 import jakarta.annotation.PostConstruct;
@@ -15,23 +17,42 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class DataInitializer {
 
+    private final RoleRepository roleRepository;
     private final SeatTypeRepository seatTypeRepository;
     private final RoomTypeRepository roomTypeRepository;
+
+    @PostConstruct
+    public void initRole() {
+        if(roleRepository.count() == 0) {
+            roleRepository.save(Role.builder()
+                    .name("ADMIN")
+                    .description("Administrator role with full access")
+                    .build());
+            roleRepository.save(Role.builder()
+                    .name("MANAGER")
+                    .description("Manager role with limited administrative access")
+                    .build());
+            roleRepository.save(Role.builder()
+                    .name("EMPLOYEE")
+                    .description("Employee role with limited employee access")
+                    .build());
+            roleRepository.save(Role.builder()
+                    .name("USER")
+                    .description("Standard user role with basic access")
+                    .build());
+        }
+    }
 
     @PostConstruct
     public void initSeatType() {
         if (seatTypeRepository.count() == 0) {
             seatTypeRepository.save(SeatType.builder()
-                            .name("Standard")
-                            .description("Standard seating with basic comfort")
-                            .build());
+                    .name("Standard")
+                    .description("Standard seating with basic comfort")
+                    .build());
             seatTypeRepository.save(SeatType.builder()
                     .name("VIP")
                     .description("VIP seating with extra comfort and amenities")
-                    .build());
-            seatTypeRepository.save(SeatType.builder()
-                    .name("Premium")
-                    .description("Premium seating with the best view and comfort")
                     .build());
             seatTypeRepository.save(SeatType.builder()
                     .name("Couple")

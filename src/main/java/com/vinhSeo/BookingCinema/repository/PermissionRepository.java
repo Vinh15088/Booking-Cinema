cdc2c7f615d694370ee8b3f2867d28ab230018cb
Vinh15088 <vinhzz1508@gmail.com>
2025-02-20 00:27:16 +0700

    delete permission repo

--- src/main/java/com/vinhSeo/BookingCinema/repository/PermissionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vinhSeo.BookingCinema.repository;
-
-import com.vinhSeo.BookingCinema.model.Permission;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PermissionRepository extends JpaRepository<Permission, Integer> {
-}

90cf7f2865e746916aafb32d39ee111a98c8d86f
Vinh15088 <vinhzz1508@gmail.com>
2025-02-20 00:26:49 +0700

    update repo

--- src/main/java/com/vinhSeo/BookingCinema/repository/SeatTypeRepository.java
+++ src/main/java/com/vinhSeo/BookingCinema/repository/SeatTypeRepository.java
@@ -2,8 +2,12 @@ package com.vinhSeo.BookingCinema.repository;
 
 import com.vinhSeo.BookingCinema.model.SeatType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SeatTypeRepository extends JpaRepository<SeatType, Integer> {
+
+    @Query("SELECT st FROM SeatType st WHERE st.id = :seatTypeId")
+    SeatType findBySeatTypeId(int seatTypeId);
 }

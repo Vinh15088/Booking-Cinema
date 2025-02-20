525575bfaf7df1790ceff64c857dfafb77770df9
Vinh15088 <vinhzz1508@gmail.com>
2025-02-20 17:16:02 +0700

    update, using async in cloudinary service

--- src/main/java/com/vinhSeo/BookingCinema/service/CloudinaryService.java
+++ src/main/java/com/vinhSeo/BookingCinema/service/CloudinaryService.java
@@ -4,12 +4,14 @@ import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 @Slf4j(topic = "CLOUDINARY_SERVICE")
@@ -18,61 +20,35 @@ public class CloudinaryService {
 
     private final Cloudinary cloudinary;
 
-    public Map<String, String> uploadFile(MultipartFile file, String folderName) throws IOException {
-        String fileName = file.getOriginalFilename();
-        String publicId = fileName.substring(0, fileName.lastIndexOf("."));
-        log.info("Uploading file with publicId: {}", publicId);
-        Map<?, ?> uploadResult =  cloudinary.uploader().upload(file.getBytes(),
-                ObjectUtils.asMap(
-                        "folder", folderName,
-                        "public_id", publicId
-                ));
+    @Async
+    public CompletableFuture<Map<String, String>> uploadFileAsync(MultipartFile file, String folderName) {
+        try {
+            String fileName = file.getOriginalFilename();
+            String publicId = fileName.substring(0, fileName.lastIndexOf("."));
+            log.info("Uploading file with publicId: {}", publicId);
+            Map<?, ?> uploadResult =  cloudinary.uploader().upload(file.getBytes(),
+                    ObjectUtils.asMap(
+                            "folder", folderName,
+                            "public_id", publicId
+                    ));
 
-        return extractUrlAndSecureUrl(uploadResult);
+            return CompletableFuture.completedFuture(extractUrlAndSecureUrl(uploadResult));
+        } catch (IOException e) {
+            log.error("Uploading file failed", e);
+            return CompletableFuture.failedFuture(e);
+        }
     }
 
-    public Map<String, String> updateFile(MultipartFile file, String folderName, String publicId) throws IOException {
-        if(file != null && !file.isEmpty()) {
-            deleteFile(publicId);
+    public CompletableFuture<Map<String, String>> deleteFile(String publicId) throws IOException {
+        try {
+            Map<String, String> result = cloudinary.uploader().destroy(publicId, ObjectUtils.emptyMap());
 
-            return uploadFile(file, folderName);
+            return CompletableFuture.completedFuture(result);
+        } catch (Exception e) {
+            log.error("Deleting file failed", e);
+            return CompletableFuture.failedFuture(e);
         }
 
-        return null;
-    }
-
-    public Map deleteFile(String publicId) throws IOException {
-        return cloudinary.uploader().destroy(publicId, ObjectUtils.emptyMap());
-    }
-
-    public Map<String, String> uploadVideo(MultipartFile file, String folderName) throws IOException {
-        String fileName = file.getOriginalFilename();
-        String publicId = fileName.substring(0, fileName.lastIndexOf("."));
-        Map<?, ?> uploadResult = cloudinary.uploader().upload(file.getBytes(),
-                ObjectUtils.asMap(
-                        "resource_type", "video",
-                        "folder", folderName,
-                        "public_id", publicId
-                ));
-
-        return extractUrlAndSecureUrl(uploadResult);
-    }
-
-    public Map<String, String> updateVideo(MultipartFile file, String folderName, String publicId) throws IOException {
-        if(file != null && !file.isEmpty()) {
-            deleteFile(publicId);
-
-            return uploadVideo(file, folderName);
-        }
-
-        return null;
-    }
-
-    public Map deleteVideo(String publicId) throws IOException {
-        return cloudinary.uploader().destroy(publicId,
-                ObjectUtils.asMap(
-                    "resource_type", "video"
-        ));
     }
 
     private Map<String, String> extractUrlAndSecureUrl(Map<?, ?> uploadResult) {

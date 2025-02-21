9071cf70110212bbadebee3cd71a013a943312bb
Vinh15088 <vinhzz1508@gmail.com>
2025-02-21 16:27:23 +0700

    add data init of movie type

--- src/main/java/com/vinhSeo/BookingCinema/utils/DataInitializer.java
+++ src/main/java/com/vinhSeo/BookingCinema/utils/DataInitializer.java
@@ -1,9 +1,11 @@
 package com.vinhSeo.BookingCinema.utils;
 
 import com.vinhSeo.BookingCinema.enums.RoomTypeStatus;
+import com.vinhSeo.BookingCinema.model.MovieType;
 import com.vinhSeo.BookingCinema.model.Role;
 import com.vinhSeo.BookingCinema.model.RoomType;
 import com.vinhSeo.BookingCinema.model.SeatType;
+import com.vinhSeo.BookingCinema.repository.MovieTypeRepository;
 import com.vinhSeo.BookingCinema.repository.RoleRepository;
 import com.vinhSeo.BookingCinema.repository.RoomTypeRepository;
 import com.vinhSeo.BookingCinema.repository.SeatTypeRepository;
@@ -20,6 +22,7 @@ public class DataInitializer {
     private final RoleRepository roleRepository;
     private final SeatTypeRepository seatTypeRepository;
     private final RoomTypeRepository roomTypeRepository;
+    private final MovieTypeRepository movieTypeRepository;
 
     @PostConstruct
     public void initRole() {
@@ -77,6 +80,48 @@ public class DataInitializer {
                     .build());
 
             log.info("Room type added to database.");
+        }
+    }
+
+    @PostConstruct
+    public void initMovieType() {
+        if(movieTypeRepository.count() == 0) {
+            movieTypeRepository.save(MovieType.builder()
+                    .name("Action")
+                    .description("Movies with intense action sequences.")
+                    .build());
+
+            movieTypeRepository.save(MovieType.builder()
+                    .name("Comedy")
+                    .description("Movies intended to make the audience laugh.")
+                    .build());
+
+            movieTypeRepository.save(MovieType.builder()
+                    .name("Drama")
+                    .description("Movies focused on emotional and character development.")
+                    .build());
+
+            movieTypeRepository.save(MovieType.builder()
+                    .name("Horror")
+                    .description("Movies designed to scare and terrify the audience.")
+                    .build());
+
+            movieTypeRepository.save(MovieType.builder()
+                    .name("Sci-Fi")
+                    .description("Movies based on speculative, futuristic, or scientific themes.")
+                    .build());
+
+            movieTypeRepository.save(MovieType.builder()
+                    .name("Romance")
+                    .description("Movies focused on love and romantic relationships.")
+                    .build());
+
+            movieTypeRepository.save(MovieType.builder()
+                    .name("Adventure")
+                    .description("Movies involving exciting journeys or quests.")
+                    .build());
+
+            log.info("Movie type added to database.");
         }
     }
 

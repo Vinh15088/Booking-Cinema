6b025c3cd8083d8e61a5066470c342a56202aa73
Vinh15088 <vinhzz1508@gmail.com>
2025-02-27 18:00:08 +0700

    add redis ticket (save ticket when user holds seat

---
+++ src/main/java/com/vinhSeo/BookingCinema/model/RedisTicket.java
@@ -0,0 +1,28 @@
+package com.vinhSeo.BookingCinema.model;
+
+import com.vinhSeo.BookingCinema.dto.request.TicketDetailRequest;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@RedisHash(value = "redis_ticket", timeToLive = 310)
+public class RedisTicket implements Serializable {
+
+    @Id
+    Integer userId;
+
+    String bookingId;
+    Integer price;
+
+    Integer showTimeId;
+    List<TicketDetailRequest> ticketDetailRequests;
+}
---
+++ src/main/java/com/vinhSeo/BookingCinema/repository/RedisTicketRepository.java
@@ -0,0 +1,9 @@
+package com.vinhSeo.BookingCinema.repository;
+
+import com.vinhSeo.BookingCinema.model.RedisTicket;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RedisTicketRepository extends CrudRepository<RedisTicket, Integer> {
+}

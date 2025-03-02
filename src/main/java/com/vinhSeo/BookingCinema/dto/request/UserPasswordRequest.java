b65c02588d5ede422fa5d44b8519b57438a0337c
Vinh15088 <vinhzz1508@gmail.com>
2025-03-02 18:22:04 +0700

    update

--- src/main/java/com/vinhSeo/BookingCinema/dto/request/UserPasswordRequest.java
+++ src/main/java/com/vinhSeo/BookingCinema/dto/request/UserPasswordRequest.java
@@ -1,8 +1,6 @@
 package com.vinhSeo.BookingCinema.dto.request;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.experimental.FieldDefaults;
@@ -12,10 +10,6 @@ import java.io.Serializable;
 @Getter
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserPasswordRequest implements Serializable {
-
-    @NotNull(message = "id must be not null")
-    @Min(value = 1, message = "id must be equals or greater than 1")
-    Integer id;
 
     @NotBlank(message = "oldPassword must be not blank")
     String oldPassword;

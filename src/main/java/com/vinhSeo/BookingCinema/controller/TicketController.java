6d5c52ac54598f56dcf0ee3162e921dc38a0176b
Vinh15088 <vinhzz1508@gmail.com>
2025-03-02 18:23:51 +0700

    update api ticket after using authentication principal

--- src/main/java/com/vinhSeo/BookingCinema/controller/TicketController.java
+++ src/main/java/com/vinhSeo/BookingCinema/controller/TicketController.java
@@ -7,20 +7,19 @@ import com.vinhSeo.BookingCinema.mapper.TicketDetailMapper;
 import com.vinhSeo.BookingCinema.mapper.TicketMapper;
 import com.vinhSeo.BookingCinema.model.RedisTicket;
 import com.vinhSeo.BookingCinema.model.Ticket;
+import com.vinhSeo.BookingCinema.model.User;
+import com.vinhSeo.BookingCinema.service.BookingService;
 import com.vinhSeo.BookingCinema.service.TicketService;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,70 +34,20 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TicketController {
 
-    @Value("${jwt.accessKey}")
-    private String ACCESS_KEY;
-
-
     private final TicketService ticketService;
     private final TicketMapper ticketMapper;
     private final TicketDetailMapper ticketDetailMapper;
+    private final BookingService bookingService;
 
-    @Operation(method = "POST", summary = "Create new ticket",
-            description = "Send a request via this API to create new ticket")
-    @PostMapping()
-    public ResponseEntity<?> createTicket(@Valid @RequestBody TicketRequest ticketRequest,
-                                          @RequestHeader("Authorization") String token
-                                          ) {
-        log.info("Create new ticket");
+    @Operation(method = "POST", summary = "Booking ticket",
+            description = "Send a request via this API to booking ticket by user")
+    @PreAuthorize("hasAnyAuthority('USER')")
+    @PostMapping("/booking")
+    public ResponseEntity<?> bookingTicket(@Valid @RequestBody TicketRequest ticketRequest,
+                                      @AuthenticationPrincipal User user) {
+        log.info("Booking ticket request: {}", ticketRequest.toString());
 
-        String jwtToken = token.replace("Bearer ", "");
-
-        Integer userId;
-
-        Claims claims = Jwts.parser()
-                .setSigningKey(Keys.hmacShaKeyFor(Decoders.BASE64.decode(ACCESS_KEY)))
-                .build()
-                .parseClaimsJws(jwtToken)
-                .getBody();
-        userId = claims.get("id", Integer.class);
-
-
-        Ticket ticket = ticketService.createTicket(userId, ticketRequest);
-
-        TicketResponse ticketResponse = ticketMapper.toTicketResponse(ticket, ticketDetailMapper);
-
-        DataApiResponse<?> dataApiResponse = DataApiResponse.builder()
-                .success(true)
-                .code(HttpStatus.CREATED.value())
-                .timestamp(new Date())
-                .message("Ticket created successfully")
-                .data(ticketResponse)
-                .build();
-        return ResponseEntity.ok().body(dataApiResponse);
-    }
-
-    @Operation(method = "POST", summary = "Hold seat",
-            description = "Send a request via this API to hold seat by user")
-    @PostMapping("/hold-seat")
-    public ResponseEntity<?> holdSeat(@Valid @RequestBody TicketRequest ticketRequest,
-                                          @RequestHeader("Authorization") String token
-    ) {
-        log.info("Hold seat");
-
-        String jwtToken = token.replace("Bearer ", "");
-
-        Integer userId;
-
-        Claims claims = Jwts.parser()
-                .setSigningKey(Keys.hmacShaKeyFor(Decoders.BASE64.decode(ACCESS_KEY)))
-                .build()
-                .parseClaimsJws(jwtToken)
-                .getBody();
-        userId = claims.get("id", Integer.class);
-
-
-        RedisTicket redisTicket = ticketService.holdSeat(userId, ticketRequest);
-
+        RedisTicket redisTicket = bookingService.bookingTicket(user.getId(), ticketRequest);
 
         DataApiResponse<?> dataApiResponse = DataApiResponse.builder()
                 .success(true)
@@ -112,37 +61,25 @@ public class TicketController {
 
     @Operation(method = "POST", summary = "Cancel hold seat",
             description = "Send a request via this API to cancel hold seat by user")
-    @PostMapping("/cancel-hold-seat")
-    public ResponseEntity<?> cancelHoldSeat(@RequestHeader("Authorization") String token
-    ) {
-        log.info("Cancel hold seat");
+    @PreAuthorize("hasAnyAuthority('USER')")
+    @PostMapping("/cancel-booking")
+    public ResponseEntity<?> cancelBooking(@AuthenticationPrincipal User user) {
+        log.info("Cancel booking ticket");
 
-        String jwtToken = token.replace("Bearer ", "");
-
-        Integer userId;
-
-        Claims claims = Jwts.parser()
-                .setSigningKey(Keys.hmacShaKeyFor(Decoders.BASE64.decode(ACCESS_KEY)))
-                .build()
-                .parseClaimsJws(jwtToken)
-                .getBody();
-        userId = claims.get("id", Integer.class);
-
-
-        ticketService.cancelHoldSeat(userId);
-
+        bookingService.cancelBooking(user.getId());
 
         DataApiResponse<?> dataApiResponse = DataApiResponse.builder()
                 .success(true)
                 .code(HttpStatus.CREATED.value())
                 .timestamp(new Date())
-                .message("Cancel hold seat successfully")
+                .message("Cancel booking ticket successfully")
                 .build();
         return ResponseEntity.ok().body(dataApiResponse);
     }
 
     @Operation(method = "GET", summary = "Get ticket by id",
             description = "Send a request via this API to get ticket by id")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'MANAGER', 'EMPLOYEE', 'USER')")
     @GetMapping("/{id}")
     public ResponseEntity<?> getTicketById(
             @PathVariable @Min(value = 1, message = "id must be greater than 0")  Integer id) {
@@ -165,11 +102,11 @@ public class TicketController {
     @Operation(method = "GET", summary = "Get all tickets or by userId, showTimeId",
             description = "Send a request via this API to all tickets or by userId, showTimeId")
     @GetMapping("/list")
-    public ResponseEntity<?> getAllTickets(@RequestParam(value = "userId", required = false) Integer userId,
+    public ResponseEntity<?> getAllTickets(@AuthenticationPrincipal User user,
                                            @RequestParam(value = "showTimeId", required = false) Integer showTimeId) {
         log.info("Get all tickets or by userId, showTimeId");
 
-        List<Ticket> ticketList = ticketService.getAllTickets(userId, showTimeId);
+        List<Ticket> ticketList = ticketService.getAllTickets(user.getId(), showTimeId);
 
         List<TicketResponse> ticketResponses = ticketList.stream().map(ticket ->
                 ticketMapper.toTicketResponse(ticket, ticketDetailMapper)).toList();
@@ -183,6 +120,5 @@ public class TicketController {
                 .build();
         return ResponseEntity.ok().body(dataApiResponse);
     }
-
 
 }
